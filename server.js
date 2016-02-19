0dbc10fb8544649f71d5fc7c60af329c6ddb5f75
yichenglee <yichenglee@gmail.com>
2016-02-19 11:36:58 +0800

    init

---
+++ server.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var jwt = require('jsonwebtoken');
+var sha1 = require('sha1');
+
+if(!fs.existsSync('user_store'))
+  fs.mkdirSync('user_store');
+
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
+app.post('/info',function(req,res){
+  var token = req.body.token;
+  jwt.verify(token,'some_magic_word',function(err,decoded){
+    if(err){
+      res.status(500);
+      res.end();
+    }else{
+      res.json({success:true,email:decoded.email});
+    }
+
+  });
+});
+
+//   jwt.verify(token,'some_magic_word',function(err,decoded){
+//     if(err) throw err;
+//     res.json({email:decoded.email})
+//   })
+// });
+
+
+app.post('/register',function(req,res){
+  var email = req.body.email
+  var password = req.body.password
+  var filepath = 'user_store/' + email;
+  fs.exists(filepath, function(exists){
+    if(exists){
+      res.json({success:false,message:'email exists'})
+    }else{
+      fs.appendFile(filepath,sha1(password),function(err){
+        if(err) throw err;
+        res.json({success:true});
+      })
+    }
+  })
+});
+
+
+
+app.post('/login',function(req,res){
+  var email = req.body.email
+  var password = req.body.password
+  var filepath = 'user_store/' + email;
+  var token;
+  fs.exists(filepath, function(exists){
+    if(exists){
+      fs.readFile(filepath,function(err,data){
+        if(err) throw err;
+        if(data == sha1(password)){
+          token = jwt.sign({email:email},'some_magic_word',{
+            expiresIn: 60
+          });
+          res.json({success:true,token:token});
+        }else{
+          res.status(401)
+          res.end()
+        }
+      });
+    }else{
+      res.status(401)
+      res.end()
+    }
+  })
+
+})
+
+
+
+
+
+// app.set('view engine','ejs');
+
+// app.get('/info',function(req,res){
+//   res.render('info');
+// });
+
+// app.post('/api/info',function(req,res){
+//   var token = req.body.token
+//   jwt.verify(token,'some_magic_word',function(err,decoded){
+//     if(err) throw err;
+//     res.json({email:decoded.email})
+//   })
+// });
+
+// app.post('/register',function(req,res){
+//
+//
+//
+//
+//   var email = req.body.email;
+//   var password = req.body.password;
+//   var token = jwt.sign(req.body,'some_magic_word',{
+//     expiresIn: 60
+//   });
+//   res.cookie('token',token);
+//   res.redirect('/info')
+//   // res.json({
+//   //   success: true,
+//   //   message: 'Enjoy your token!',
+//   //   token: token
+//   // });
+//
+//
+//
+// });
+app.listen(3000);
